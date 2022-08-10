be55a62547bd670614fb07f528ae6bb213b110f8
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-08-10 16:49:17 +0200

    upgrade versions of xWiki and postgresql connector
    add commented code for workaround for slashes for imported pages from
    confluence

--- templates/setenv.sh
+++ templates/setenv.sh
@@ -9,3 +9,8 @@ PATH=$JAVA_HOME/bin:$PATH
 if [ -z "$JAVA_OPTS" ]; then
 	JAVA_OPTS="-Djava.awt.headless=true"
 fi
+
+# allow slashes in page names, that have been imported from confluence
+# see https://jira.xwiki.org/browse/CONFLUENCE-61
+# see https://www.xwiki.org/xwiki/bin/view/Documentation/AdminGuide/Installation/InstallationWAR/InstallationTomcat/#HAllowing222F22and225C22inpagenames
+# CATALINA_OPTS=-Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true
